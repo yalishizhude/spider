fc5d311183bd12c7bb3daeccc1a9301747f2e384
Delong Zhu <Delong Zhu>
2016-11-08 22:26:58 +0800

    web

--- app.js
+++ app.js
@@ -15,18 +15,28 @@ var pSchema = mongoose.Schema({
   request: Array,
   resposibility: Array
 });
-var datestamp = (new Date()).toISOString().split('T').shift().replace(/-/g,'_');
-var pModel = mongoose.model('position'+ datestamp, pSchema);
-var random = 1000 * 10 * Math.random();
+var datestamp = (new Date()).toISOString().split('T').shift().replace(/-/g, '_');
+var pModel = mongoose.model('position' + datestamp, pSchema);
+var random = 1000 * 5 * Math.random();
+var keyword = '前端';
+var history = [];
 
 function query(pn) {
-  if (!pn) return;
-  request('http://www.lagou.com/jobs/positionAjax.json?px=default&yx=50k以上&needAddtionalResult=false', {
+  pn = pn || 1;
+  request('http://www.lagou.com/jobs/positionAjax.json?px=default&yx=25k-50k&needAddtionalResult=false', {
+    method: 'POST',
     form: {
-      first: 1===pn,
+      first: 1 === pn,
       pn: pn || 1,
-      kd: '技术总监'
-    }
+      kd: keyword
+    },
+    headers: [{
+      name: 'Content-Type',
+      value: 'application/x-www-form-urlencoded',
+    }, {
+      name: 'User-Agent',
+      value: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    }]
   }, function(err, res, body) {
     if (err) console.error(err);
     var obj = JSON.parse(body).content;
@@ -34,11 +44,12 @@ function query(pn) {
     var total = obj.positionResult.totalCount;
     var result = obj.positionResult.result;
     var jobDef = [];
-    console.log('当前页%s,%s条数据', pn, result.length);
+    console.log('共%s条数据，当前页%s,%s条数据', total, pn, result.length);
     result.forEach(function(item) {
       var def = q.defer();
       jobDef.push(def.promise);
-      if(/技术总监|CTO|cto/.test(item.positionName)){
+      if (item.positionName.indexOf(keyword) > -1 && history.indexOf(item.positionId) < 0) {
+        history.push(item.positionId);
         setTimeout(function(position) {
           queryById(position).then(function(job) {
             var position = new pModel(job);
@@ -81,16 +92,26 @@ function queryById(position) {
       var request = text.split(/职位要求/).pop().split('\n');
       var resposibility = text.split(/岗位职责/).pop().split(/职位要求/).shift().split('\n');
       resposibility.forEach(function(str) {
-        if(str) position.resposibility.push(str);
+        if (str) position.resposibility.push(str);
       });
       request.forEach(function(str) {
-        if(str) position.request.push(str);
+        if (str) position.request.push(str);
       });
       console.log('当前职位', position);
       def.resolve(position);
     });
   return def.promise;
 }
-pModel.remove(function(){
-  query(1);
+// pModel.remove(query);
+pModel.find({request: /angular/ig}).exec(function(err, data) {
+  console.log('angular: %s', data.length);
+});
+pModel.find({request: /react/ig}).exec(function(err, data) {
+  console.log('react: %s', data.length);
+});
+pModel.find({request: /vue/ig}).exec(function(err, data) {
+  console.log('vue: %s', data.length);
+});
+pModel.find({request: /jquery/ig}).exec(function(err, data) {
+  console.log('jquery: %s', data.length);
 });
